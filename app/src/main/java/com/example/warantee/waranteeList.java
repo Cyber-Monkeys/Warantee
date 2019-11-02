e454a8ac123c5672aea8738b032cedd15d9d9665
Humaid Khan <humaidk2@gmail.com>
2019-11-03 00:02:25 +0400

    Create warantee list to list all warantees

---
+++ app/src/main/java/com/example/warantee/waranteeList.java
@@ -0,0 +1,14 @@
+package com.example.warantee;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class waranteeList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_warantee_list);
+    }
+}
