e38c25e5e1f76b28ac43212da6e4f848372aea82
Mahmoud <b00037739@aus.edu>
2019-11-19 18:12:45 +0400

    Added second addwarantee form. The page contians adding Video and Image

---
+++ app/src/main/java/com/example/warantee/AddWaranteeForm2.java
@@ -0,0 +1,67 @@
+package com.example.warantee;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import static com.example.warantee.AddWaranteeForm.REQUEST_IMAGE_CAPTURE;
+
+public class AddWaranteeForm2 extends AppCompatActivity {
+
+    ImageView warrantyImage;
+    Button submit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.content_add_warentee_form_2);
+
+        //Button and  ImageView for the Camera
+        Button captureImage = (Button) findViewById(R.id.captureImage);
+        warrantyImage = (ImageView) findViewById(R.id.warrantyImage);
+
+        //Check if the phone has a camera
+        if (!hasCamera())
+            captureImage.setEnabled(false);
+
+        //Create submission button
+        submit = (Button) findViewById(R.id.FormSubmitButton);
+
+    }
+
+
+    //----------------------------- Camera Functions -----------------------------
+    //has a camera function
+    private boolean hasCamera() {
+        return getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_ANY); // to check if the phone has a camera,
+        // if not it will disable the button
+    }
+
+    //Launching the Camera
+    public void launchCamera(View view) {
+        //This intent is to to lunch the camera when this function is called
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        //after taking the image we want to save so we start an activity to save the image
+        startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
+    }
+
+    //Receiving the Image and displaying it in ImageView
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap Photo = (Bitmap) extras.get("data");
+            warrantyImage.setImageBitmap(Photo);
+        }
+    }
+    //----------------------------- End Of Camera -----------------------------
+}
