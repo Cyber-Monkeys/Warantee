a26e141d318bdc7fe216347210af2d470b0c31ed
fidellim <lim.fidel@gmail.com>
2019-11-26 22:52:58 +0400

    initialize textviews

--- app/src/main/java/com/example/warantee/WarrantyInfo.java
+++ app/src/main/java/com/example/warantee/WarrantyInfo.java
@@ -3,12 +3,23 @@ package com.example.warantee;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class WarrantyInfo extends AppCompatActivity {
 
+    TextView sellerName, sellerPhone, sellerEmail, amount, date, warrantyPeriod, category;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_warranty_info);
+
+        sellerName = findViewById(R.id.seller_name);
+        sellerPhone = findViewById(R.id.seller_phone);
+        sellerEmail = findViewById(R.id.seller_email);
+        amount = findViewById(R.id.amount);
+        date = findViewById(R.id.date);
+        warrantyPeriod = findViewById(R.id.warranty_period);
+        category = findViewById(R.id.category);
+
     }
 }
